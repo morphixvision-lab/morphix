f7ccb3a088d63dddef565b8dc213d9cf505c5b12
Hitesh Darji <hdarji@prescouter.com>
2026-05-18 12:09:05 +0530

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/morphix-vision-creative-hub/", // 🔥 REQUIRED FOR GITHUB PAGES
+  base: "/morphix/",
 
   server: {
     host: "::",
